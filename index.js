085340a50c1b984cb863ec1697e25b5740f210fe
Aidana777 <aidananiazalieva27@gmail.com>
2023-02-23 21:10:15 +0600

    image

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const url = './db.json';
-const xhr = new XMLHttpRequest();
+let http = new XMLHttpRequest();
+http.open('get', 'db.json', true);
+http.send();
+http.onload = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let products = JSON.parse(this.responseText);
+        let output = '';
+        for (let item of products) {
+            output += `
+            <div class="product">
 
-xhr.open(url)
-xhr.send();
-xhr.addEventListener('load', () => {
-    if (xhr.readyState === 5 && xhr.status === 200) {
-        let result = JSON.parse(xhr.response)
-        console.log(result);
+            <img src="${item.image}" alt="${item.image}">
+            <div class="price">
+                <p class="current__price"> ${item.price}  </p>
+                <p class="old__price"></p>
+                <p class="title">${item.title}<span  class="desribtion">${item.desribtion}</span></p>
+      
+            </div>
+        </div>
+            `;
+        }
+        document.querySelector('.products').innerHTML = output
     }
-})
+}
